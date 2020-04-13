51d8917ca5019b7eb1d963c883dd330bb348333b
Jeff Bullock <thetanbull@gmail.com>
2020-04-13 19:27:11 -0400

    Changes db vars to env vars

--- queries.js
+++ queries.js
@@ -2,17 +2,17 @@ const Pool = require('pg').Pool
 
 // TODO - Add dotenv + environment variables
 const pool = new Pool({
-  user: '',
-  host: 'localhost',
-  database: '',
-  password: '',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 })
 
 
 // TEMP (Sanity Check) ******************************
 const getUrls = (req, res) => {
-  pool.query('SELECT * FROM urls ORDER BY id ASC', (err, results) => {
+  pool.query('SELECT * FROM urls ORDER BY url_id ASC', (err, results) => {
     if (err) {
       throw err
     }
@@ -25,14 +25,14 @@ const getUrls = (req, res) => {
 const getRedirect = (req, res) => {
   const redirectString = req.params.redirect;
   
-  pool.query('SELECT * FROM urls WHERE redirect = $1', [redirectString], (err, results) => {
+  pool.query('SELECT * FROM urls WHERE redirect_string = $1', [redirectString], (err, results) => {
     if (err) {
       throw err
     }
     const queryResults = results.rows;
 
     console.log(queryResults[0])
-    res.redirect(302, `https://${queryResults[0].link}`)
+    res.redirect(302, `${queryResults[0].redirect_link}`)
   })
 }
 
