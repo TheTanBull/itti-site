189c640e5a916bf31b837783061463b55eccc3f1
Jeffrey Bullock <thetanbull@gmail.com>
2020-04-23 14:48:07 -0400

    Adds HTTPS

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const https = require('https');
 const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const db = require('./queries');
 
 const PORT = process.env.PORT;
@@ -19,17 +21,27 @@ app.use(
 );
 app.use('/static/', express.static('public'));
 
-// TODO - Create Homepage
+
+// **** ROUTES ****
 app.get('/', (req, res) => {
   res.render('home', { host: process.env.HOST });
 });
 
 app.get('/:redirect([A-z0-9]{3,})', db.getRedirect);
 
+
 // **** API ****
 app.post('/api/urls', db.minifyUrl);
 
 
+// **** SERVER ****
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
 });
+
+https.createServer({
+  key: fs.readFileSync(process.env.HTTPS_KEY),
+  cert: fs.readFileSync(process.env.HTTPS_CERT)
+}, app).listen(8000, function() {
+  console.log('Running HTTPS on port 8000');
+})
