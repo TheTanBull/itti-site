9a64d8ac3234b9db59e08c5d789c6749c59f2729
Jeffrey Bullock <thetanbull@gmail.com>
2020-04-24 07:34:16 -0400

    Adds 404 Redirect

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ const db = require('./queries');
 
 const PORT = process.env.PORT;
 
+// Template Engine
 app.set('view engine', 'ejs');
 
+
+// Middlewares
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -26,6 +29,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:redirect([A-z0-9]{3,})', db.getRedirect);
+
+// Non-Existent Routing
+app.use(function(req, res, next){
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.redirect('/');
+  next(err);
+});
 
 
 // **** API ****
